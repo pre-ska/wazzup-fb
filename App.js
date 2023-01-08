3bd2ad5226d73b58208a79040a2c6b8323c3358d
preska <wolfwar@outlook.com>
2023-01-08 09:06:44 +0100

    tab icons

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import * as Font from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import ChatListScreen from './screens/ChatListScreen';
 import ChatSettingsScreen from './screens/ChatSettingsScreen';
@@ -27,7 +28,9 @@ const TabNavigator = () => {
         component={ChatListScreen}
         options={{
           tabBarLabel: 'Chats',
-          // tabBarIcon: ({ color, size }) => {}
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubble-outline" size={24} color="black" />
+          ),
         }}
       />
       <Tab.Screen
@@ -35,7 +38,9 @@ const TabNavigator = () => {
         component={SettingsScreen}
         options={{
           tabBarLabel: 'Settings',
-          // tabBarIcon: ({ color, size }) => {}
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={24} color="black" />
+          ),
         }}
       />
     </Tab.Navigator>
